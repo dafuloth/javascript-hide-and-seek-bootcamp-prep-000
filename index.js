07eddfe4a3687d9263a0feb8d5a8c3888aac6ed1
David Thai <david.thai@dafuloth.me>
2018-09-01 01:08:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+
+function deepestChild() {
+  const allChildren = document.getElementById('grand-node').querySelectorAll('*')
+
+  // Simplest Solution:
+  return allChildren[allChildren.length-1]
+}
+
+
+function increaseRankBy(n) {
+  let rankedLi = document.querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < rankedLi.length; i++) {
+    //rankedLi[i].innerText = parseInt(rankedLi.innerHTML) + parseInt(n)
+    let rank = parseInt(rankedLi[i].textContent)
+    rank = rank + parseInt(n)
+  }
+}
